159e476a270ffe1d14bbc483d02d8ab14c126e99
Frasan00 <francesco.sangiovanni.7@gmail.com>
2023-12-03 13:53:28 +0100

    feature: Finished model manager (to be tested)

--- src/Mysql/Models/QueryTemplates/SET.ts
+++
@@ -1,9 +0,0 @@
-const setTemplate = (table: string) => {
-  return {
-    set: (column: string, value: string) => `\nSET ${column} = ${value} `,
-    setMulti: (columns: string[], values: string[]) =>
-      `\nSET ${columns.join(", ")} = ${values.join(", ")} `,
-  };
-};
-
-export default setTemplate;
