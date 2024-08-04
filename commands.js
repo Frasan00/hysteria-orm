7c56038bd5c3dfa75fe08e033e6558357bb8b2ea
Frasan00 <francesco.sangiovanni.7@gmail.com>
2024-08-04 10:41:05 +0200

    commander.js

--- commands.js
+++ commands.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
-const { createMigrationSql } = require('./src/hysteria-cli/mysql/create-migration');
-const { runMigrationsSql } = require('./src/hysteria-cli/mysql/run-migration');
-const { migrationRollBackSql } = require('./src/hysteria-cli/mysql/rollback-migration');
+const migrationCreateConnector = require('./src/hysteria-cli/migrationCreateConnector.ts').default;
+const migrationRunConnector = require('./src/hysteria-cli/migrationRunConnector.ts').default;
+const migrationRollbackConnector = require('./src/hysteria-cli/migrationRollbackConnector.ts').default;
 
 const program = new Command();
 
@@ -11,21 +11,21 @@ program
   .command('create-migration <name>')
   .description('Create a new migration')
   .action((name) => {
-    createMigrationSql(name);
+    migrationCreateConnector(name);
   });
 
 program
   .command('run-migrations')
   .description('Run all pending migrations')
   .action(async () => {
-    await runMigrationsSql();
+    await migrationRunConnector();
   });
 
 program
   .command('rollback-migration')
   .description('Rollbacks all migrations')
   .action(async () => {
-    await migrationRollBackSql();
+    await migrationRollbackConnector();
   });
 
 program.parse(process.argv);
