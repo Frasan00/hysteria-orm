dc22301bc9dd7fcd674080f7cea86aaf506bf0b0
Frasan00 <francesco.sangiovanni.7@gmail.com>
2025-04-12 10:58:20 +0200

    updated SQL_README

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,36 @@ import {
 import { createModelFactory } from "./sql/models/model_factory";
 import { QueryBuilder } from "./sql/query_builder/query_builder";
 import { SqlDataSource } from "./sql/sql_data_source";
+import { UserWithUuid } from "../test/sql/test_models/uuid/user_uuid";
 import logger from "./utils/logger";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+(async () => {
+  await SqlDataSource.connect({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "root",
+    password: "root",
+    database: "test",
+    logs: true,
+  });
+  const users = await UserWithUuid.query()
+    .where("name", "1")
+    .orWhere("name", "2")
+    .whereBuilder((qb) => {
+      qb.where("name", "1")
+        .orWhere("name", "2")
+        .orWhereSubQuery("id", "IN", (qb) => {
+          qb.select("id").from("users").where("name", "1");
+        });
+    })
+    .toQuery();
+  console.log(users);
+  await SqlDataSource.disconnect();
+})();
 
 export default {
   // decorators
