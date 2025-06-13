b416a006b556b4510c43226df56205a612b9416e
Frasan00 <francesco.sangiovanni.7@gmail.com>
2025-06-13 23:07:26 +0200

    chore: refactor index.ts for improved exports and organization
    - Streamlined exports in index.ts by grouping related functionalities such as decorators, models, data sources, migrations, and utilities.
    - Removed unused imports to enhance code clarity and maintainability.
    - Improved type exports for MongoDataSource, RedisDataSource, and SqlDataSource for better type safety.

--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,35 @@
-import type { DataSourceInput } from "./data_source/data_source_types";
-import type {
-  RedisFetchable,
-  RedisStorable,
-} from "./no_sql/redis/redis_data_source";
-import type { ClientMigrator } from "./sql/migrations/migrator";
-import type { ModelQueryBuilder } from "./sql/models/model_query_builder/model_query_builder";
-import type { RelationQueryBuilderType } from "./sql/models/model_query_builder/relation_query_builder/relation_query_builder_types";
-import type { MongoDataSource } from "./no_sql/mongo/mongo_data_source";
-import type { RedisDataSource } from "./no_sql/redis/redis_data_source";
-import type { SqlDataSource } from "./sql/sql_data_source";
+// Decorators
+export { property } from './no_sql/mongo/mongo_models/mongo_collection_decorators';
+export { belongsTo, column, hasMany, hasOne, manyToMany } from './sql/models/decorators/model_decorators';
 
-import { HysteriaError } from "./errors/hysteria_error";
-import { MongoDataSource as mongo } from "./no_sql/mongo/mongo_data_source";
-import { Collection } from "./no_sql/mongo/mongo_models/mongo_collection";
-import {
-  getCollectionProperties,
-  property,
-} from "./no_sql/mongo/mongo_models/mongo_collection_decorators";
-import { RedisDataSource as redis } from "./no_sql/redis/redis_data_source";
-import { Migration } from "./sql/migrations/migration";
-import { defineMigrator } from "./sql/migrations/migrator";
-import {
-  belongsTo,
-  column,
-  getModelColumns,
-  getPrimaryKey,
-  getRelations,
-  hasMany,
-  hasOne,
-  manyToMany,
-} from "./sql/models/decorators/model_decorators";
-import { Model } from "./sql/models/model";
-import { createModelFactory } from "./sql/models/model_factory";
-import { QueryBuilder } from "./sql/query_builder/query_builder";
-import { SqlDataSource as sql } from "./sql/sql_data_source";
-import logger from "./utils/logger";
-import { withPerformance } from "./utils/performance";
-import { generateULID } from "./utils/ulid";
-import { generateKeyPair } from "./utils/encryption";
+// Models & Factories
+export { Model } from './sql/models/model';
+export { createModelFactory } from './sql/models/model_factory';
+export type { ModelQueryBuilder } from './sql/models/model_query_builder/model_query_builder';
+export type { RelationQueryBuilderType } from './sql/models/model_query_builder/relation_query_builder/relation_query_builder_types';
 
-export {
-  // decorators
-  belongsTo,
-  ClientMigrator,
-  Collection,
-  column,
-  // factory
-  createModelFactory,
-  // DataSource
-  DataSourceInput,
-  defineMigrator,
-  // utils
-  generateULID,
-  getCollectionProperties,
-  getModelColumns,
-  getPrimaryKey,
-  getRelations,
-  hasMany,
-  hasOne,
-  // Errors
-  HysteriaError,
-  // logger
-  logger,
-  manyToMany,
-  // migrations
-  Migration,
-  // sql
-  Model,
-  ModelQueryBuilder,
-  RelationQueryBuilderType,
-  // mongo
-  mongo,
-  property,
-  QueryBuilder,
-  // redis
-  redis,
-  RedisFetchable,
-  RedisStorable,
-  sql,
-  withPerformance,
-  generateKeyPair,
-  // types
-  MongoDataSource,
-  RedisDataSource,
-  SqlDataSource,
-};
+// DataSources
+export type { DataSourceInput } from './data_source/data_source_types';
+export { MongoDataSource as mongo } from './no_sql/mongo/mongo_data_source';
+export type { MongoDataSource } from './no_sql/mongo/mongo_data_source';
+export { RedisDataSource as redis } from './no_sql/redis/redis_data_source';
+export type { RedisDataSource, RedisFetchable, RedisStorable } from './no_sql/redis/redis_data_source';
+export { SqlDataSource as sql } from './sql/sql_data_source';
+export type { SqlDataSource } from './sql/sql_data_source';
 
-export default {
-  // decorators
-  belongsTo,
-  Collection,
-  column,
-  property,
-  hasMany,
-  hasOne,
-  manyToMany,
-  // logger
-  logger,
+// Migrations
+export { Migration } from './sql/migrations/migration';
+export { defineMigrator } from './sql/migrations/migrator';
+export type { ClientMigrator } from './sql/migrations/migrator';
 
-  // utils
-  getCollectionProperties,
-  getModelColumns,
-  getPrimaryKey,
-  getRelations,
+// Query Builder
+export { QueryBuilder } from './sql/query_builder/query_builder';
 
-  // migrations
-  Migration,
-  defineMigrator,
+// Utils
+export { default as logger } from './utils/logger';
 
-  // sql
-  Model,
-  sql,
-  QueryBuilder,
+// Errors
+export { HysteriaError } from './errors/hysteria_error';
 
-  // mongo
-  mongo,
-
-  // redis
-  redis,
-
-  // factory
-  createModelFactory,
-
-  // Errors
-  HysteriaError,
-};
+// Mongo
+export { Collection } from './no_sql/mongo/mongo_models/mongo_collection';
